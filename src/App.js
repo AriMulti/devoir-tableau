92ee8c8d8f8c427091d12eba65fff89b7a9c764f
AriMulti <2245630@cstj.qc.ca>
2024-09-05 14:33:48 -0400

    Version01

--- src/App.js
+++ src/App.js
@@ -4,8 +4,38 @@ export default class App {
 	 * Méthode principale. Sera appelée après le chargement de la page.
 	 */
 	static main() {
-		var tableau1, tableau2, tableau3;
+		var table, thead, tbody, tr, td;
 		var app = document.getElementById("app");
 		
+		table = document.createElement("table");
+		app.appendChild("table");
+
+		thead = document.createElement("thead");
+		table.appendChild("thead");
+		thead.innerHTML = "Undertale Goats!";
+
+	// <table>
+	// 	<thead>
+	// 		Undertale Goats!
+	// 	</thead>
+	// 	<tbody>
+	// 		<tr>
+	// 			<td>Toriel</td>
+	// 			<td>Asgore</td>
+	// 			<td>Asriel</td>
+	// 		</tr>
+	// 		<tr>
+	// 			<td>Dreemurr</td>
+	// 			<td>Dreemurr</td>
+	// 			<td>Dreemurr</td>
+	// 		</tr>
+	// 		<tr>
+	// 			<td>Mauve et blanc</td>
+	// 			<td>Mauve et jaune</td>
+	// 			<td>Jaune et vert</td>
+	// 		</tr>
+	// 	</tbody>
+	// </table>
+
 	}
 }
